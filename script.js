66b7a94511db81e3f329b2a3bc85d1c99d3a6093
PieCruiser <piecruiser@gmail.com>
2024-05-02 21:36:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// TypeScript
+var typing = new Typed(".text", {
+  strings: ["", "the car", "the robot"],
+  typeSpeed: 100,
+  backSpeed: 40,
+  loop: true
+});
+
+// GSAP
+gsap.registerPlugin(ScrollTrigger);
+const introsplitTypes = document.querySelectorAll(".left-part h1");
+introsplitTypes.forEach((char, i) => {
+  const i_text = new SplitType(char);
+  gsap.to(i_text.chars, {
+    y: 0,
+    stagger: 0.08, // text splitting transition
+    duration: 0.3 // full text duration
+  });
+});
