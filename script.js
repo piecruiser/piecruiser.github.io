705f11b4f0a16c5dc719f15936f225178bd7dc08
PieCruiser <piecruiser@gmail.com>
2024-05-04 09:09:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
-// TypeScript
+document.getElementById("myButton").addEventListener("click", function() {
+  this.classList.add("animate");
+  setTimeout(function() {
+    window.location.href = "https://piecruiser.github.io";
+  }, 1000);
+  });
+
+function scrollToNextSection() {
+  var specialSection = document.querySelector('.special-section');
+  specialSection.scrollIntoView({ behavior: 'smooth' });
+}
+  
 var typing = new Typed(".text", {
-  strings: ["", "the car", "the robot"],
+  strings: ["", "the car.", "the robot."],
   typeSpeed: 100,
   backSpeed: 40,
   loop: true
 });
 
-// GSAP
 gsap.registerPlugin(ScrollTrigger);
 const introsplitTypes = document.querySelectorAll(".left-part h1");
 introsplitTypes.forEach((char, i) => {
   const i_text = new SplitType(char);
   gsap.to(i_text.chars, {
     y: 0,
-    stagger: 0.08, // text splitting transition
-    duration: 0.3 // full text duration
+    stagger: 0.08,
+    duration: 0.3 
   });
 });
